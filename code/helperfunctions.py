2abdbada32aaf7c43c89b94a302585f6d7208602
Jessica Dafflon <jejedafflon@gmail.com>
2020-08-21 10:51:07 +0200

    Improve helperfunctions and avoid prints
    * Move dissimilarity analysis, posterioronlyModels functions into the helperfunction
    * Copy the gateway_coef_sign function into the helperfunction and comment out a print function. This avoids unnecessary prints.
    * Remove brutforce analysis of the dissimilarity analysis. This code was used to make sure that the distribution generated using the formula was correct

--- code/helperfunctions.py
+++ code/helperfunctions.py
@@ -1,27 +1,184 @@
 from itertools import product
 
 import numpy as np
-from sklearn.preprocessing import PolynomialFeatures, KBinsDiscretizer
-from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomTreesEmbedding
-from sklearn.pipeline import make_pipeline
+from sklearn.neighbors import kneighbors_graph
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn import manifold
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from numpy.random import seed
-from mpl_toolkits.mplot3d import Axes3D
 import bct
-from sklearn_rvm import EMRVR
-from sklearn.svm import LinearSVR, LinearSVC, SVR, SVC
-from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR, SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
-from scipy.stats import spearmanr
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
+from scipy.stats import hypergeom
 
-def objectiveFunc(TempModelNum,Y,Sparsities_Run,Data_Run,BCT_models,BCT_Run,CommunityIDs,data1,data2,ClassOrRegress):
+# Set the random seed
+np.random.seed(2)
+rng = np.random.default_rng(2)
+
+
+def gateway_coef_sign(W, ci, centrality_type='degree'):
+    '''
+    The gateway coefficient is a variant of participation coefficient.
+    It is weighted by how critical the connections are to intermodular
+    connectivity (e.g. if a node is the only connection between its
+    module and another module, it will have a higher gateway coefficient,
+    unlike participation coefficient).
+    Parameters
+    ----------
+    W : NxN np.ndarray
+        undirected signed connection matrix
+    ci : Nx1 np.ndarray
+        community affiliation vector
+    centrality_type : enum
+        'degree' - uses the weighted degree (i.e, node strength)
+        'betweenness' - uses the betweenness centrality
+    Returns
+    -------
+    Gpos : Nx1 np.ndarray
+        gateway coefficient for positive weights
+    Gneg : Nx1 np.ndarray
+        gateway coefficient for negative weights
+    Reference:
+        Vargas ER, Wahl LM, Eur Phys J B (2014) 87:1-10
+
+    Note
+    ----
+    This function was copied from the bctpy package. The main diffrence is that
+    line 84 was commented out to avoid unnecessary printing.
+    '''
+    _, ci = np.unique(ci, return_inverse=True)
+    ci += 1
+    n = len(W)
+    np.fill_diagonal(W, 0)
+
+    def gcoef(W):
+        #strength
+        s = np.sum(W, axis=1)
+        #neighbor community affiliation
+        Gc = np.inner((W != 0), np.diag(ci))
+        #community specific neighbors
+        Sc2 = np.zeros((n,))
+        #extra modular weighting
+        ksm = np.zeros((n,))
+        #intra modular wieghting
+        centm = np.zeros((n,))
+
+        if centrality_type == 'degree':
+            cent = s.copy()
+        elif centrality_type == 'betweenness':
+            cent = bct.betweenness_wei(invert(W))
+
+        nr_modules = int(np.max(ci))
+        for i in range(1, nr_modules+1):
+            ks = np.sum(W * (Gc == i), axis=1)
+            #print(np.sum(ks))
+            Sc2 += ks ** 2
+            for j in range(1, nr_modules+1):
+                #calculate extramodular weights
+                ksm[ci == j] += ks[ci == j] / np.sum(ks[ci == j])
+
+            #calculate intramodular weights
+            centm[ci == i] = np.sum(cent[ci == i])
+
+        #print(Gc)
+        #print(centm)
+        #print(ksm)
+        #print(ks)
+
+        centm = centm / max(centm)
+        #calculate total weights
+        gs = (1 - ksm * centm) ** 2
+
+        Gw = 1 - Sc2 * gs / s ** 2
+        Gw[np.where(np.isnan(Gw))] = 0
+        Gw[np.where(np.logical_not(Gw))] = 0
+
+        return Gw
+
+    G_pos = gcoef(W * (W > 0))
+    G_neg = gcoef(-W * (W < 0))
+    return G_pos, G_neg
+
+
+def get_dissimilarity_n_neighbours(all_neighbours_orig,
+                                   all_neighbours_reduced):
+    '''
+    Calculate the dissimilarity
+
+    Parameters
+    ----------
+    all_neighbours_orig:
+    all_neighbours_reduced:
+
+    Returns
+    -------
+    all_dissimilarity: Dissimilarity scores between the original and reduced
+    space
+    '''
+
+    all_dissimilarity = []
+    for K in range(len(all_neighbours_reduced)):
+        # Find the set of different indices
+        diff = set(sorted(all_neighbours_orig[K])) - \
+               set(sorted(all_neighbours_reduced[K]))
+        # Calculate the dissimilarity
+        epsilon = len(diff) / len(all_neighbours_orig[K])
+        all_dissimilarity.append(epsilon)
+
+    return all_dissimilarity
+
+
+def get_models_neighbours(N, n_neighbors_step, data):
+    '''
+    Calculate the dissimilarity
+
+    Parameters
+    ----------
+    n_neighbours: number of neighbours to analyse
+    data: data (pairwise_subjects, n_analysis)
+
+    Returns
+    -------
+    all_dissimilarity: Dissimilarity scores between the original and reduced
+    space
+    '''
+    n_neighbours = range(2, N, n_neighbors_step)
+    all_adj = np.zeros((len(data), len(data), len(n_neighbours)))
+    all_neighbours_orig = []
+
+    for idx, n_neighbour in enumerate(n_neighbours):
+        adj = kneighbors_graph(data, n_neighbour, mode='distance',
+                            metric='euclidean')
+        adj_array = adj.toarray()
+        all_adj[:, :, idx] = adj_array
+        nneighbours_orig = np.nonzero(adj_array)
+        nneighbours_orig = [item for item in zip(nneighbours_orig[0],
+                                                 nneighbours_orig[1])]
+        all_neighbours_orig.append(nneighbours_orig)
+
+    return all_neighbours_orig, all_adj
+
+
+def get_null_distribution(N, n_neighbors_step):
+    # Calculate the null distribution using binary distribution
+    def expectation(N, K):
+        rv = hypergeom(N, K, K)
+        x = np.arange(0, K)
+        pmf = rv.pmf(x)
+        return np.sum(x*pmf)
+
+    null_distribution = []
+    for K in range(2, N, n_neighbors_step):
+        E = expectation(N, K)
+        diss = 1 - (E/K)
+        null_distribution.append(diss)
+    return null_distribution
+
+
+def objectiveFunc(TempModelNum, Y, Sparsities_Run, Data_Run, BCT_models, BCT_Run,
+                  CommunityIDs, data1, data2, ClassOrRegress):
     '''
 
     Define the objective function for the Bayesian optimization.  This consists
@@ -29,92 +186,113 @@ def objectiveFunc(TempModelNum,Y,Sparsities_Run,Data_Run,BCT_models,BCT_Run,Comm
     control which subjects are tested, a random permutation of the indices of the
     subjects, the predictor variables and the actual y outcomes, the number of
     subjects to include in each iteration
+
+    Parameters
+    ----------
+        TempModelNum: idx of the analysis being run
+        Y: Y variable that will be predicted
+        Sparsities_Run: List of threshold used
+        Data_Run: Data used for creating the space
+        BCT_models: Dictionary containing the list of models used
+        BCT_Run: List containing the order in which the BCT models were run
+        CommunityIDs: Information about the Yeo network Ids
+        data1: Motion Regression functional connectivity data of the subjects
+               that were not used to create the space
+        data2: Global Signal Regression data for the subjects that were not used
+               to create the space
+        ClassOrRegress: Define if it is a classification or regression problem
+        (0: classification; 1 regression)
+
+    Returns
+    -------
+        score: Returns the MAE of the predictions
     '''
 
-    
-    if Data_Run[TempModelNum]==0:
-        TempData=data1
-        TotalRegions=346
-        TotalSubjects=TempData.shape[2]
-    elif Data_Run[TempModelNum]==1:
-        TempData=data2
-        TotalRegions=346
-        TotalSubjects=TempData.shape[2]
-    
+    TotalRegions = 346
+    if Data_Run[TempModelNum] == 'MRS':
+        TempData = data1
+    elif Data_Run[TempModelNum] == 'GRS':
+        TempData = data2
+    else:
+        ValueError('This type of pre-processing is not supported')
+    TotalSubjects = TempData.shape[2]
 
-    
-    TempThreshold=Sparsities_Run[TempModelNum]
-    
-    BCT_Num=[i for i, e in enumerate(BCT_models) if e[0] == BCT_Run[TempModelNum]][0]
-    
-    TempResults=np.zeros([TotalSubjects,TotalRegions])
-    for SubNum in range(0,TotalSubjects):
-        x = bct.threshold_proportional(TempData[:,:,SubNum], TempThreshold, copy=True)
-        if BCT_Num==7:
-            s=np.asarray(BCT_models[BCT_Num][1](x,1))
-        elif BCT_Num==8:
-            temp_s=np.asarray(BCT_models[BCT_Num][1](x))
-            s=temp_s[0]
-        elif BCT_Num==9:
-            temp_s=np.asarray(BCT_models[BCT_Num][1](x))
-            s=temp_s[0]
-        elif BCT_Num==10:
-            s=np.asarray(BCT_models[BCT_Num][1](x,CommunityIDs))
-        elif BCT_Num==11:
-            s=np.asarray(BCT_models[BCT_Num][1](x,CommunityIDs))
-        elif BCT_Num==12:
-            s=np.asarray(BCT_models[BCT_Num][1](x,0.85))
-                #elif BCT_Num==13:
-                #   temp_s=np.asarray(BCT_models[BCT_Num][1](x))
-                #   s=temp_s[0]
-        elif BCT_Num==13:
-                #temp_s=np.asarray(BCT_models[BCT_Num][1](x,KeptYeoIDs,'degree'))
-            temp_s=np.asarray(BCT_models[BCT_Num][1](x,CommunityIDs))
-            s=temp_s[0] 
-        elif BCT_Num==14:
-            temp_s=np.asarray(BCT_models[BCT_Num][1](x,CommunityIDs))
-            s=temp_s[0] 
-        elif BCT_Num==15:
-            temp_s=np.asarray(BCT_models[BCT_Num][1](x))
-            s=temp_s[0]
+    TempThreshold = Sparsities_Run[TempModelNum]
+    BCT_Num = BCT_Run[TempModelNum]
+
+    TempResults = np.zeros([TotalSubjects, TotalRegions])
+    for SubNum in range(0, TotalSubjects):
+        x = bct.threshold_proportional(TempData[:, :, SubNum],
+                                       TempThreshold, copy=True)
+        if BCT_Num == 'local efficiency':
+            ss = BCT_models[BCT_Num](x, 1);
+        elif BCT_Num == 'modularity (louvain)':
+            temp = BCT_models[BCT_Num](x);
+            ss = temp[0]
+        elif BCT_Num== 'modularity (probtune)':
+            temp = BCT_models[BCT_Num](x);
+            ss = temp[0]
+        elif BCT_Num == 'participation coefficient':
+            ss = BCT_models[BCT_Num](x, CommunityIDs);
+        elif BCT_Num == 'module degree z-score':
+            ss = BCT_models[BCT_Num](x, CommunityIDs);
+        elif BCT_Num == 'pagerank centrality':
+            ss = BCT_models[BCT_Num](x, 0.85)
+        elif BCT_Num == 'diversity coefficient':
+            temp = BCT_models[BCT_Num](x, CommunityIDs)
+            ss = temp[0]
+        elif BCT_Num == 'gateway degree':
+            temp = BCT_models[BCT_Num](x, CommunityIDs)
+            ss = temp[0]
+        elif BCT_Num == 'k-core centrality':
+            temp = BCT_models[BCT_Num](x)
+            ss = temp[0]
         else:
-            s=np.asarray(BCT_models[BCT_Num][1](x))
+            ss = BCT_models[BCT_Num](x)
+        #For each subject for each approach keep the 346 regional values.
+        TempResults[SubNum, :] = ss
 
-        TempResults[SubNum,:]=s 
     scaler = StandardScaler()
-    TempResults=scaler.fit_transform(TempResults)
+    TempResults = scaler.fit_transform(TempResults)
 
-    
-
-
-    RandInt=np.random.randint(10000)
-    print(RandInt)
-    #model = EMRVR(kernel="linear")
-    if ClassOrRegress==1:
-        #model = LinearSVR(random_state=0, tol=1e-5)
+    RandInt = np.random.randint(10000)
+    #print(RandInt)
+    if ClassOrRegress == 1:
         model = SVR(C=1.0, epsilon=0.2)
         cv = KFold(n_splits=10, shuffle=True, random_state=RandInt)
-        scores = cross_val_score(model, TempResults, Y.ravel(), cv=cv,scoring='neg_mean_absolute_error')
-        score=scores.mean()/10
-        
-    else: 
+        scores = cross_val_score(model, TempResults, Y.ravel(), cv=cv,
+                                 scoring='neg_mean_absolute_error')
+        # Note: the scores were divided by 10 in order to keep the values close
+        # to 0 for avoiding problems with the Bayesian Optimisation
+        score = scores.mean()/10
+
+    else:
         model = SVC(gamma='auto')
         cv = StratifiedKFold(n_splits=10, random_state=RandInt, shuffle=True)
-        scores = cross_val_score(model, TempResults, Y.ravel(), cv=cv,scoring='neg_mean_absolute_error')
-        score=scores.mean()/10
-    
-    
+        scores = cross_val_score(model, TempResults, Y.ravel(), cv=cv,
+                                 scoring='neg_mean_absolute_error')
+        score = scores.mean()/10
+
     return score
 
 
-def _posterior(gp, x_obs, y_obs, z_obs, grid_X):
+def posterior(gp, x_obs, y_obs, z_obs, grid_X):
     xy = (np.array([x_obs.ravel(), y_obs.ravel()])).T
     gp.fit(xy, z_obs)
     mu, std = gp.predict(grid_X.reshape(-1, 2), return_std=True)
     return mu, std, gp
 
 
-def display_gp_mean_uncertainty(kernel, optimizer, pbounds,BadIter):
+# Helper function for calculating posterior predictions only for points
+# in the space where an analysis approach exists
+def posteriorOnlyModels(gp, x_obs, y_obs, z_obs, AllModelEmb):
+    xy = (np.array([x_obs.ravel(), y_obs.ravel()])).T
+    gp.fit(xy, z_obs)
+    mu, std = gp.predict(AllModelEmb, return_std=True)
+    return mu, std, gp
+
+
+def display_gp_mean_uncertainty(kernel, optimizer, pbounds, BadIter):
     '''
     Code to display the estimated GP regression mean across the space as well
     as the uncertainty, showing which points were sampled.
@@ -137,8 +315,6 @@ def display_gp_mean_uncertainty(kernel, optimizer, pbounds,BadIter):
     y_obs=y_temp[BadIter==0]
     z_obs=z_temp[BadIter==0]
 
-
-    
     x1x2 = np.array(list(product(x, y)))
     X0p, X1p = x1x2[:, 0].reshape(50, 50), x1x2[:, 1].reshape(50, 50)
 
@@ -200,17 +376,13 @@ def display_gp_mean_uncertainty(kernel, optimizer, pbounds,BadIter):
     return gp
 
 
-def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter, pbounds, nbrs,RandomSeed,ModelEmbedding,BCT_models,BCT_Run,Sparsities_Run,Data_Run,Ages,CommunityIDs,data1,data2,ClassOrRegress,MultivariateUnivariate):
+def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter,
+                          pbounds, nbrs, RandomSeed, ModelEmbedding, BCT_models,
+                          BCT_Run, Sparsities_Run, Data_Run, Ages, CommunityIDs,
+                          data1, data2, ClassOrRegress, MultivariateUnivariate):
 
     BadIters=np.empty(0)
-
-
-
-
-
     LastModel=-1
-    
-
     Iter=0
     while Iter < init_points + n_iter:
         np.random.seed(RandomSeed+Iter)
@@ -236,8 +408,10 @@ def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter, pboun
         # find the index of the models that are closest to this point
         distances, indices = nbrs.kneighbors(Model_coord)
 
-        # I order to reduce repeatedly sampling the same point, check if suggested point was sampled last and then check in ModelNums what the name/index
-        # of that model is, if was recently sampled then take the second nearest point. 
+        # I order to reduce repeatedly sampling the same point, check if
+        # suggested point was sampled last and then check in ModelNums what the
+        # name/index of that model is, if was recently sampled then take the
+        # second nearest point.
         if LastModel == np.asscalar(indices[0][0]):
             TempModelNum = np.asscalar(indices[0][1])
             ActualLocation = ModelEmbedding[np.asscalar(indices[0][1])]
@@ -246,15 +420,19 @@ def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter, pboun
             TempModelNum = np.asscalar(indices[0][0])
             ActualLocation = ModelEmbedding[np.asscalar(indices[0][0])]
             Distance=distances[0][0]
-        
 
-       
-        if (Distance <10 or Iter<init_points):   #Hack: because space is continuous but analysis approaches aren't, we penalize points that are far (>10 distance in model space) from any actual analysis approaches by assigning them the value of the worst performing approach in the burn-in
+        if (Distance <10 or Iter<init_points):
+            #Hack: because space is continuous but analysis approaches aren't,
+            # we penalize points that are far (>10 distance in model space)
+            #from any actual analysis approaches by assigning them the value of
+            # the worst performing approach in the burn-in
             LastModel = TempModelNum
             BadIters=np.append(BadIters,0)
         # Call the objective function and evaluate the model/pipeline
             if MultivariateUnivariate<0:
-                target=objectiveFunc(TempModelNum,Ages,Sparsities_Run,Data_Run,BCT_models,BCT_Run,CommunityIDs,data1,data2,ClassOrRegress)
+                target=objectiveFunc(TempModelNum,Ages,Sparsities_Run,Data_Run,
+                                     BCT_models,BCT_Run,CommunityIDs,data1,
+                                     data2,ClassOrRegress)
                 print("Next Iteration")
                 print(Iter)
                 # print("Model Num %d " % TempModelNum)
@@ -262,7 +440,7 @@ def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter, pboun
                 print(Distance)
                 print("Target Function: %.4f" % (target))
                 print(' ')
-                seed(Iter)
+                np.random.seed(Iter)
                 # This is a hack. Add a very small random number to the coordinates so
                 # that even if the model has been previously selected the GP thinks its
                 # a different point, since this was causing it to crash
@@ -272,25 +450,25 @@ def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter, pboun
                 target=objectiveFuncUnivariate(TempModelNum,Ages,Sparsities_Run,Data_Run,BCT_models,BCT_Run,CommunityIDs,data1,data2,MultivariateUnivariate)
                 print("Next Iteration")
                 print(Iter)
-                
+
                 print('Print indices: %d  %d' % (indices[0][0], indices[0][1]))
                 print(Distance)
                 print("Target Function: %.4f" % (target))
                 print(' ')
-                
-                seed(Iter)
+
+                np.random.seed(Iter)
                 # This is a hack to add very small random number to the coordinates so
                 # that even if the model has been previously selected the GP thinks its
                 # a different point, since this was causing it to crash
                 TempLoc1 = ActualLocation[0] + (np.random.random_sample(1) - 0.5)/10
                 TempLoc2 = ActualLocation[1] + (np.random.random_sample(1) - 0.5)/10
 
-        else: 
-            
+        else:
+
             newlist = sorted(optimizer.res, key=lambda k: k['target'])
             target=newlist[0]['target']
             LastModel = -1
-            
+
             print("Next Iteration")
             print(Iter)
             # print("Model Num %d " % TempModelNum)
@@ -298,13 +476,13 @@ def bayesian_optimisation(kernel, optimizer, utility, init_points, n_iter, pboun
             print(Distance)
             print("Target Function Default Bad: %.4f" % (target))
             BadIters=np.append(BadIters,1)
-            
+
             print(' ')
-            seed(Iter)
+            np.random.seed(Iter)
             TempLoc1 = Model_coord[0][0]
             TempLoc2 = Model_coord[0][1]
             n_iter=n_iter+1
-    
+
         Iter=Iter+1
 
         # Update the GP data with the new coordinates and model performance
